7b0b5cb8c4832def2fc8bc140e681d7ab96e0fc5
Srujan-rai <raisrujan@gmail.com>
2024-02-29 21:10:15 +0530

    login page push

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import mysql.connector
-from flask import Flask,render_template,redirect,url_for
+from flask import Flask,render_template,redirect,url_for,request
 from mysql.connector import Error
 app=Flask(__name__)
 db={
@@ -18,6 +18,12 @@ except Error as e:
     
 cursor=connection.cursor()
 
+users = {
+    'user1': 'password1',
+    'user2': 'password2',
+    'user3': 'password3'
+}
+
 @app.route('/')
 def home():
 
@@ -32,7 +38,15 @@ def user():
 def admin():
     return "welcome admin"
 
-
+@app.route('/login',methods=["GET","POST"])
+def login():
+    if request.method=="POST":
+        username=request.form["username"]
+        password=request.form["password"]
+        if username in users and users[username]==password:
+            return redirect(url_for("home"))
+    else:
+        return render_template("login.html")
 
 
 if __name__=="__main__":
